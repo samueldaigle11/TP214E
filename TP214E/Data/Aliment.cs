1370fb41a2048e05460d253425944be0a2a11eec
samueldaigle11 <samueldaigle@live.ca>
2021-10-28 16:45:10 -0400

    Ajout des attributs, changement des get/set, reste à vérifier quel get et quel set est nécessaire. Ajout d'un TODO pour les get/set

--- TP214E/Data/Aliment.cs
+++ TP214E/Data/Aliment.cs
@@ -7,11 +7,38 @@ namespace TP214E.Data
 {
     public class Aliment
     {
-        public ObjectId Id { get; set; }
-        public string Nom { get; set; }
-        public int Quantite { get; set; }
-        public string Unite { get; set; }
-        public DateTime ExpireLe { get; set; }
+        private ObjectId _id;
+        private string _nom;
+        private int _quantite;
+        private string _unite;
+        private DateTime _datePeremption;
+
+        // TODO VERIFIER SI ON GARDE LES GET SET OU SI ON LAISSE JUSTE GET A CERTAINS ENDROITS ( GARDER JUSTE LES GET ET LES SETS REQUIS)
+        public ObjectId Id 
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string Nom 
+        {
+            get { return _nom; }
+            set { _nom = value; }
+        }
+        public int Quantite 
+        {
+            get { return _quantite; }
+            set { _quantite = value; }
+        }
+        public string Unite 
+        {
+            get { return _unite; }
+            set { _unite = value; }
+        }
+        public DateTime DatePeremption 
+        {
+            get { return _datePeremption; }
+            set { _datePeremption = value; }
+        }
 
     }
 }
