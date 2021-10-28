62e5fa178c7e16a2a97bd0fecfb9da967e7fff96
samueldaigle11 <samueldaigle@live.ca>
2021-10-28 16:08:34 -0400

    Refactoring general de DAL.cs et conversion en AccesseurBaseDeDonnees.cs

---
+++ TP214E/Data/AccesseurBaseDeDonnees.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using MongoDB.Driver;
+
+namespace TP214E.Data
+{
+    public class AccesseurBaseDeDonnees
+    {
+        private MongoClient clientMongoDB;
+        public AccesseurBaseDeDonnees()
+        {
+            clientMongoDB = OuvrirConnexion();
+        }
+
+        public List<Aliment> ObtenirAliments()
+        {
+            List<Aliment> aliments = new List<Aliment>();
+            try
+            {
+                IMongoDatabase baseDeDonnees = clientMongoDB.GetDatabase("TP2DB");
+                aliments = baseDeDonnees.GetCollection<Aliment>("Aliments").Aggregate().ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Impossible de se connecter à la base de données " + ex.Message, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
+            return aliments;
+        }
+
+        private MongoClient OuvrirConnexion()
+        {
+            MongoClient clientBaseDeDonnees = null;
+            try
+            {
+                clientBaseDeDonnees = new MongoClient("mongodb://localhost:27017/TP2DB");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Impossible de se connecter à la base de données " + ex.Message, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            return clientBaseDeDonnees;
+        }
+
+    }
+}
