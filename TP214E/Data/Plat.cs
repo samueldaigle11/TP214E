3346198e694b2d11e426caa798e9656621e0826a
JHuard <bboy_arrow@hotmail.com>
2021-11-10 11:42:55 -0500

    Retirer l'attribut objetsInventaireNecessaires de la classe Plat

--- TP214E/Data/Plat.cs
+++ TP214E/Data/Plat.cs
@@ -9,13 +9,13 @@ namespace TP214E.Data
         private ObjectId _id;
         private string _nom;
         private double _prix;
-        private List<(string, int)> _objetsInventaireNecessaires;
-        public Plat(string pNom, double pPrix, List<(string, int)> pObjetsInventaireNecessaires)
+
+        public Plat(string pNom, double pPrix)
         {
             Nom = pNom;
             Prix = pPrix;
-            ObjetsInventaireNecessaires = pObjetsInventaireNecessaires;
         }
+
         public ObjectId Id
         {
             get { return _id; }
@@ -26,16 +26,13 @@ namespace TP214E.Data
             get { return _nom; }
             set { _nom = value; }
         }
+
         public double Prix
         {
             get { return _prix; }
             set { _prix = value; }
         }
-        public List<(string, int)> ObjetsInventaireNecessaires
-        {
-            get { return _objetsInventaireNecessaires; }
-            set { _objetsInventaireNecessaires = value; }
-        }
+
         public override string ToString()
         {
             return $"{Nom} {Prix}$";
